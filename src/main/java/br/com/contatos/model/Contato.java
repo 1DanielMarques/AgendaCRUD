51f7b1f4ca46cc6323e1b69bde693334d4264522
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-14 17:32:27 -0300

    Ajustes nomes

---
+++ src/main/java/br/com/contatos/model/Contato.java
@@ -0,0 +1,65 @@
+package br.com.contatos.model;
+
+import java.util.Date;
+
+/**
+ * @author mDanielSE
+ */
+public class Contato {
+
+    private int id;
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String profissao;
+    private Date dataCadastro;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+}
